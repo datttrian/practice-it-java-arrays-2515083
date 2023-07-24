a6d599424fc98852b4288c0c94665ead9608e9b7
Dat T. Tran <datttrian@gmail.com>
2023-07-24 10:04:10 +0000

    Reverse an array in place

--- src/_11/App.java
+++ src/_11/App.java
@@ -3,9 +3,15 @@ package _11;
 import java.util.Arrays;
 
 public class App {
-
+  // 1,2,3,4
+  // 4,2,3,1
+  // 4,3,2,1
   public static void reverse(int[] arr) {
-
+    for (int i = 0; i < arr.length / 2; i++) {
+      int temp = arr[i];
+      arr[i] = arr[arr.length - 1 - i];
+      arr[arr.length - 1 - i] = temp;
+    }
   }
 
   public static void main(String args[]) {
